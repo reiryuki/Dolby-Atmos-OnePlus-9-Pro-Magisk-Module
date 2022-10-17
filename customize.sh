47730cfe153c2df2a925b1ffcfb2e1757083b4d2
Riki Irawan <lightningthunder9999@gmail.com>
2022-10-17 02:06:38 +0000

    Replace customize.sh

--- customize.sh
+++ customize.sh
@@ -191,6 +191,38 @@ for NAMES in $NAME; do
   blockdev --setrw $DIR$NAMES
 done
 }
+remount_rw() {
+DIR=/dev/block/bootdevice/by-name
+NAME="/vendor$SLOT /cust$SLOT /system$SLOT /system_ext$SLOT"
+set_read_write
+DIR=/dev/block/mapper
+set_read_write
+DIR=$MAGISKTMP/block
+NAME="/vendor /system_root /system /system_ext"
+set_read_write
+mount -o rw,remount $MAGISKTMP/mirror/system
+mount -o rw,remount $MAGISKTMP/mirror/system_root
+mount -o rw,remount $MAGISKTMP/mirror/system_ext
+mount -o rw,remount $MAGISKTMP/mirror/vendor
+mount -o rw,remount /system
+mount -o rw,remount /
+mount -o rw,remount /system_root
+mount -o rw,remount /system_ext
+mount -o rw,remount /vendor
+}
+remount_ro() {
+if [ "$BOOTMODE" == true ]; then
+  mount -o ro,remount $MAGISKTMP/mirror/system
+  mount -o ro,remount $MAGISKTMP/mirror/system_root
+  mount -o ro,remount $MAGISKTMP/mirror/system_ext
+  mount -o ro,remount $MAGISKTMP/mirror/vendor
+  mount -o ro,remount /system
+  mount -o ro,remount /
+  mount -o ro,remount /system_root
+  mount -o ro,remount /system_ext
+  mount -o ro,remount /vendor
+fi
+}
 backup() {
 if [ ! -f $FILE.orig ] && [ ! -f $FILE.bak ]; then
   cp -f $FILE $FILE.orig
@@ -219,7 +251,7 @@ if [ -f $FILE ]; then
     </hal>' $FILE
     ui_print " "
   else
-    ui_print "! Failed to create"
+    ui_print "- Failed to create"
     ui_print "$FILE.orig"
     ui_print " "
   fi
@@ -239,7 +271,7 @@ if [ -f $FILE ]; then
 vendor.dolby.hardware.dms::IDms u:object_r:hal_dms_hwservice:s0' $FILE
     ui_print " "
   else
-    ui_print "! Failed to create"
+    ui_print "- Failed to create"
     ui_print "$FILE.orig"
     ui_print " "
   fi
@@ -351,33 +383,45 @@ for NAMES in $NAME; do
       && [ ! -f $DES ]; then
         ui_print "  ! $DES"
         ui_print "    installation failed."
+        ui_print "  Using $NAMES systemlessly."
+        cp -f $MODPATH/system_support/lib/$NAMES $MODPATH/system/lib
       fi
       if [ -f $MODPATH/system_support/lib64/$NAMES ]\
       && [ ! -f $DES2 ]; then
         ui_print "  ! $DES2"
         ui_print "    installation failed."
+        ui_print "  Using $NAMES systemlessly."
+        cp -f $MODPATH/system_support/lib64/$NAMES $MODPATH/system/lib64
       fi
       if [ -f $MODPATH/system_support/vendor/lib/$NAMES ]\
       && [ ! -f $DES3 ]; then
         ui_print "  ! $DES3"
         ui_print "    installation failed."
+        ui_print "  Using $NAMES systemlessly."
+        cp -f $MODPATH/system_support/vendor/lib/$NAMES $MODPATH/system/vendor/lib
       fi
       if [ -f $MODPATH/system_support/vendor/lib64/$NAMES ]\
       && [ ! -f $DES4 ]; then
         ui_print "  ! $DES4"
         ui_print "    installation failed."
+        ui_print "  Using $NAMES systemlessly."
+        cp -f $MODPATH/system_support/vendor/lib64/$NAMES $MODPATH/system/vendor/lib64
       fi
-      ui_print " "
     else
       ui_print "! $NAMES not found."
-      ui_print "  This module will not be working without $NAMES."
-      ui_print "  You can type:"
+      ui_print "  Using $NAMES systemlessly."
+      cp -f $MODPATH/system_support/lib/$NAMES $MODPATH/system/lib
+      cp -f $MODPATH/system_support/lib64/$NAMES $MODPATH/system/lib64
+      cp -f $MODPATH/system_support/vendor/lib/$NAMES $MODPATH/system/vendor/lib
+      cp -f $MODPATH/system_support/vendor/lib64/$NAMES $MODPATH/system/vendor/lib64
+      ui_print "  If this module still doesn't work, type:"
       ui_print "  install.hwlib=1"
       ui_print "  inside $OPTIONALS"
-      ui_print "  and reinstalling this module"
+      ui_print "  and reinstall this module"
       ui_print "  to install $NAMES directly to this ROM."
-      ui_print " "
+      ui_print "  DwYOR!"
     fi
+    ui_print " "
   fi
 done
 }
@@ -454,23 +498,7 @@ if [ "`grep_prop permissive.mode $OPTIONALS`" == 1 ]; then
 fi
 
 # remount
-DIR=/dev/block/bootdevice/by-name
-NAME="/vendor$SLOT /cust$SLOT /system$SLOT /system_ext$SLOT"
-set_read_write
-DIR=/dev/block/mapper
-set_read_write
-DIR=$MAGISKTMP/block
-NAME="/vendor /system_root /system /system_ext"
-set_read_write
-mount -o rw,remount $MAGISKTMP/mirror/system
-mount -o rw,remount $MAGISKTMP/mirror/system_root
-mount -o rw,remount $MAGISKTMP/mirror/system_ext
-mount -o rw,remount $MAGISKTMP/mirror/vendor
-mount -o rw,remount /system
-mount -o rw,remount /
-mount -o rw,remount /system_root
-mount -o rw,remount /system_ext
-mount -o rw,remount /vendor
+remount_rw
 
 # early init mount dir
 early_init_mount_dir
@@ -478,7 +506,7 @@ early_init_mount_dir
 # find
 chcon -R u:object_r:system_lib_file:s0 $MODPATH/system_support/lib*
 chcon -R u:object_r:same_process_hal_file:s0 $MODPATH/system_support/vendor/lib*
-NAME=`ls $MODPATH/system_support/vendor/lib`
+NAME="libhidltransport.so libhwbinder.so"
 find_file
 
 # function
@@ -493,32 +521,38 @@ if ! grep -Eq $NAME $DIR$FILE; then
   if [ "`grep_prop change.hidlbase $OPTIONALS`" == 1 ]\
   && [ "$API" -ge 30 ]; then
     sed -i 's/^change.hidlbase=1/change.hidlbase=0/' $OPTIONALS
-    ui_print "  Installing new"
-    ui_print "$DIR$FILE..."
+    ui_print "  Installing new libhidlbase.so..."
     ui_print "  If your device reboot automatically, then install this"
     ui_print "  module again after reboot."
     sleep 10
+    cp -f $MODPATH/system_support/vendor$FILE $DIR2$FILE
+    if ! grep -Eq $NAME $DIR2$FILE; then
+      ui_print "  ! $DIR2$FILE"
+      ui_print "    installation failed."
+      ui_print "  Using new libhidlbase.so systemlessly."
+      cp -f $MODPATH/system_support/vendor$FILE $MODPATH/system/vendor$FILE
+    fi
     cp -f $MODPATH/system_support$FILE $DIR$FILE
     if ! grep -Eq $NAME $DIR$FILE; then
-      ui_print "  ! Installation failed."
-      if [ $EIM == true ]; then
-        ui_print "  Installing new /system$FILE systemlessly"
-        ui_print "  using early init mount..."
-        mkdir -p $EIMDIR/system/$FILE
-        cp -f $MODPATH/system_support$FILE $EIMDIR/system/$FILE
-      else
-        ui_print "  Using new /system$FILE systemlessly."
-        cp -f $MODPATH/system_support$FILE $MODPATH/system/$FILE
-      fi
+      ui_print "  ! $DIR$FILE"
+      ui_print "    installation failed."
+      ui_print "  Using new libhidlbase.so systemlessly."
+      cp -f $MODPATH/system_support$FILE $MODPATH/system$FILE
     fi
   else
-    if [ "$API" -lt 30 ]; then
-      ui_print "  Unsupported ROM."
-      abort
-    else
-      ui_print "  Using new /system$FILE systemlessly."
-      cp -f $MODPATH/system_support$FILE $MODPATH/system/$FILE
+    if [ "$API" -ge 30 ]; then
+      ui_print "  Using new libhidlbase.so systemlessly."
+      cp -f $MODPATH/system_support/vendor$FILE $MODPATH/system/vendor$FILE
+      cp -f $MODPATH/system_support$FILE $MODPATH/system$FILE
+      ui_print "  If this module still doesn't work, type:"
+      ui_print "  change.hidlbase=1"
+      ui_print "  inside $OPTIONALS"
+      ui_print "  and reinstall this module"
+      ui_print "  to install new libhidlbase.so directly to this ROM."
+      ui_print "  DwYOR!"
     fi
+    remount_ro
+    abort
   fi
 fi
 ui_print " "
@@ -527,8 +561,10 @@ ui_print " "
 # check
 if [ "$BOOTMODE" == true ]; then
   DIR=`realpath $MAGISKTMP/mirror/system`
+  DIR2=`realpath $MAGISKTMP/mirror/vendor`
 else
   DIR=`realpath /system`
+  DIR2=`realpath /vendor`
 fi
 if [ "$IS64BIT" == true ]; then
   FILE=/lib64/libhidlbase.so
@@ -639,17 +675,7 @@ if ! grep -Eq 'u:object_r:hal_dms_hwservice:s0|u:object_r:default_android_hwserv
 fi
 
 # remount
-if [ "$BOOTMODE" == true ]; then
-  mount -o ro,remount $MAGISKTMP/mirror/system
-  mount -o ro,remount $MAGISKTMP/mirror/system_root
-  mount -o ro,remount $MAGISKTMP/mirror/system_ext
-  mount -o ro,remount $MAGISKTMP/mirror/vendor
-  mount -o ro,remount /system
-  mount -o ro,remount /
-  mount -o ro,remount /system_root
-  mount -o ro,remount /system_ext
-  mount -o ro,remount /vendor
-fi
+remount_ro
 
 # function
 hide_oat() {
